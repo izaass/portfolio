980e707a27ebe4dcfd14d0fe4995739a531bb66b
izaass <mr.izaass@gmail.com>
2023-11-20 13:06:17 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: index.js
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 03/06/2023
+*/
+
+// Logo
+import bl33hIcon from "./logo/bl33hIcon.png";
+
+// Technologies
+import htmlIcon from "./tech/html.png";
+import cssIcon from "./tech/css.png";
+import jsIcon from "./tech/javascript.png";
+import reactIcon from "./tech/reactjs.png";
+import awsIcon from "./tech/aws.png";
+import javaIcon from "./tech/java.png";
+import gitIcon from "./tech/git.png";
+import githubIcon from "./tech/github.png";
+import figmaIcon from "./tech/figma.png";
+import psqlIcon from "./tech/postgresql.png";
+import eslintIcon from "./tech/eslint.png";
+import viteIcon from "./tech/vite.svg";
+import pyIcon from "./tech/py.png";
+import nodeIcon from "./tech/node.webp";
+import neoIcon from "./tech/neo.png";
+import raspIcon from "./tech/raspi.png";
+import close from "./close.svg";
+import menu from "./menu.svg";
+
+// Projects
+import Expense from "./projects/Expense.jpg";
+import todo from "./projects/todo.png";
+import music from "./projects/music.png";
+
+// Avatar
+import avatar from "./avatar/avatar.png";
+
+export {
+  menu,
+  close,
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  eslintIcon,
+  figmaIcon,
+  psqlIcon,
+  nodeIcon,
+  pyIcon,
+  viteIcon,
+  neoIcon,
+  raspIcon,
+  music,
+  Expense,
+  todo,
+  avatar,
+  bl33hIcon,
+};
